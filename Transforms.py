f53f39e82f75239ecab20e7d9312c49c13911c5f
pangpangju <joo990319@naver.com>
2025-03-18 09:06:24 +0900

    Inverse Matrix

--- Transforms.py
+++ Transforms.py
@@ -4,9 +4,9 @@ import numpy as np
 def get_rotation_matrix(channel, angle_deg):
     """
     채널에 존재하는 각도를 회전 행렬로 바꿔주는 함수입니다.
-    @param channel: 바꿀 채널
-    @param angle_deg: 각도 input
-    @return: 회전 행렬
+    :param channel: 바꿀 채널
+    :param angle_deg: 각도 input
+    :return: 회전 행렬
     """
     theta = np.deg2rad(angle_deg)
     if "Xrotation" in channel:
@@ -36,8 +36,8 @@ def get_rotation_matrix(channel, angle_deg):
 def translation_matrix(offset):
     """
     Translation (x,y,z)를 행렬로 바꿔주는 함수입니다.
-    @param offset: (x,y,z) translation vector
-    @return: translation 행렬
+    :param offset: (x,y,z) translation vector
+    :return: translation 행렬
     """
     tx, ty, tz = offset
     return np.array([
@@ -51,9 +51,9 @@ def compute_forward_kinetics(node, rotations):
     """
     각 joint별로 Forward Kinemetic을 구현하기 위한 행렬입니다.
     Joint에 저장되어있는 local translation 과 rotation을 적용합니다.
-    @param node: 적용할 Node (Joint)
-    @param rotations: rotation 값
-    @return: Forward Kinetic을 적용한 4x4 행렬
+    :param node: 적용할 Node (Joint)
+    :param rotations: rotation 값
+    :return: Forward Kinetic을 적용한 4x4 행렬
     """
     M = translation_matrix(node.offset)
     channels = node.channels[-3:]  # 마지막 채널 3개가 rotation값
@@ -66,8 +66,8 @@ def extract_yaw_rotation(kinetics):
     """
     회전행렬에서 yaw값만을 추출하기 위한 함수입니다.
     XZ평면에 사영된 root Transform T 에 y축 회전을 적용하느넫 사용됩니다.
-    @param kinetics: 적용되고 있는 회전
-    @return: yaw값만을 담고있는 4x4 행렬
+    :param kinetics: 적용되고 있는 회전
+    :return: yaw값만을 담고있는 4x4 행렬
     """
     forward = kinetics[:3, 2].copy()
     forward[1] = 0  # Project onto XZ plane.
@@ -125,3 +125,18 @@ def add_motion(node, motion_frame, idx=[0]):
 
     for child in node.children:
         add_motion(child, motion_frame, idx)
+
+def inverse_matrix(T):
+    """
+    역행렬 구해주는 함수
+    :param T: 역행렬을 구할 행렬
+    :return: T의 역행렬
+    """
+    R = T[:3, :3]
+    t = T[:3, 3]
+    R_inv = R.T
+    t_inv = -R_inv @ t
+    T_inv = np.eye(4)
+    T_inv[:3, :3] = R_inv
+    T_inv[:3, 3] = t_inv
+    return T_inv
